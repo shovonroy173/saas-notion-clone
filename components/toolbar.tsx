d285ae48b6d357057a3f4d17a89fcbb3b0849461
shovonroy173 <shovonroy2003@gmail.com>
2024-08-06 17:22:28 +0530

    mod: toolbar Id modified

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -7,6 +7,7 @@ import TextareaAutosize from "react-textarea-autosize";
 
 // import { useCoverImage } from "@/hooks/use-cover-image";
 import { Doc } from "@/convex/_generated/dataModel";
+import { Id } from "@/convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 
@@ -15,6 +16,7 @@ import { IconPicker } from "./icon-picker";
 interface ToolbarProps {
   initialData: Doc<"documents">;
   preview?: boolean;
+  // documentId: Id<"documents">
 };
 
 import {
